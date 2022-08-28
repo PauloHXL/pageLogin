701cdf1faa4705e91fc89f1acc2fb48f1aa62de5
PauloHXL <paulo.xl2009@gmail.com>
2022-08-28 12:44:00 -0300

    config

---
+++ page-login/src/componets/Body.tsx
@@ -0,0 +1,11 @@
+import * as styles from './Body.module.css';
+
+export function Body() {
+    return (
+        <div className={styles.Body}>
+            <p>Oláa</p>
+        </div>
+    );
+}
+
+
